d1ffa14fb3474611c9e1c8ac3dd216aa6c52bcdc
abrahaomendes <abraaomneto@gmail.com>
2024-05-20 16:17:30 -0300

    Multicriteria query

--- src/main/java/com/workshopmongo/services/PostService.java
+++ src/main/java/com/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +22,11 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
