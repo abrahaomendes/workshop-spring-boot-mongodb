4895fbbdfcebcbf4008b814a58a61f80fb761e4b
abrahaomendes <abraaomneto@gmail.com>
2024-05-20 14:28:36 -0300

    Create entity Post

--- src/main/java/com/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/workshopmongo/repository/PostRepository.java
@@ -4,10 +4,10 @@ package com.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.workshopmongo.domain.User;
+import com.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 
 
 }
